ff46c184da3f8caf7a924196bae3759441d29c24
luangelsdorf <luanferreira2136@gmail.com>
2021-12-11 14:07:06 -0300

    Added LinkTree Page

---
+++ pages/linktree.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LinkTree from '../src/components/LinkTree';
+import NavBar from '../src/components/NavBar';
+
+export default function Links() {
+  return (
+    <>
+      <LinkTree />
+    </>
+  )
+}
---
+++ src/components/LinkTree/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from 'styled-components';
+import links from '../../../links.json';
+import ButtonPd from "../buttonPd";
+
+export const Container = styled.section`
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    max-width: 180px;
+    display: block;
+    margin: 50px auto;
+    margin: 
+  }
+
+  a {
+    margin: 10px 0;
+    button {
+      width: 400px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    a {
+      button {
+        width: 230px;
+      }
+    }
+  }
+`
+
+export default function LinkTree() {
+  return (
+    <Container>
+      <img src="/img/nav-logo.svg" alt="Zero27 Logo" />
+      {
+        links.map((link, index) => {
+          return (
+            <a target="_blank" key={`link-${index}`} href={link.href} rel="noopener noreferrer">
+              <ButtonPd>{ link.name }</ButtonPd>
+            </a>
+          )
+        })
+      }
+    </Container>
+  )
+}
